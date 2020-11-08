b0fcad8cedc03ba94f3709fbeddcf2db0954623e
unknown <jmillot41@gmail.com>
2020-11-08 01:45:30 +0100

    change id

--- catalog/selenium_tests.py
+++ catalog/selenium_tests.py
@@ -3,12 +3,16 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 PATH = 'C:\Program Files (x86)\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://purbeurre-jilvo.herokuapp.com/")
 driver.maximize_window
 print(driver.title)
+
+connection = driver.find_
+
 
 search = driver.find_element_by_id("query")
 search.send_keys("steak")
@@ -17,10 +21,10 @@ print(search.send_keys(Keys.RETURN))
 link = driver.find_element_by_id('choice_product_input')
 link.click()
 
-search = driver.find_elements_by_class_id('produit_choosen_result')
-print(search)
+search = driver.find_elements_by_id('produit_choosen_result')
+print()
 
-link = driver.find_elements_b
+# link = driver.find_elements_b
 
 
 
