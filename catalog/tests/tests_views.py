5bbd1173e2b180a44810e8dca20581628dcdcd63
unknown <jmillot41@gmail.com>
2020-11-19 17:23:50 +0100

    change test

--- catalog/tests/tests_views.py
+++ catalog/tests/tests_views.py
@@ -73,31 +73,6 @@ class NoReverse(TestCase):
 class PageTestCase(TestCase):
     """class PageTestCase"""
 
-    def test_legal_mention(self):
-        """test_legal_mention"""
-        response = self.client.get(reverse('legal_mention'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_login_page(self):
-        """test_login_page"""
-        response = self.client.get(reverse('login'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_signup_page(self):
-        """test_signup_page"""
-        response = self.client.get(reverse('signup'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_404_page(self):
-        """test_404_page"""
-        response = self.client.get(reverse('404'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_500_page(self):
-        """test_500_page"""
-        response = self.client.get(reverse('500'))
-        self.assertEqual(response.status_code, 200)
-
     def test_index_page(self):
         """test_index_page"""
         response = self.client.get(reverse("index"))
@@ -112,24 +87,6 @@ class PageTestCase(TestCase):
         """test_search_result_page"""
         response = self.client.get(reverse("search_result"))
         self.assertEqual(response.status_code, 200)
-
-class StatusTestCase(TestCase):
-    """ class StatusTestCase """
-
-    # def test_register(self):
-    #     """test_register"""
-
-
-    def test_login(self):
-        """test_login """
-        response = self.client.post(reverse('index'),
-                                    {'username': 'testuser', 'password': 'password'}, follow=True)
-        self.assertEqual(response.status_code, 200)
-    
-    def test_invalid_user(self):
-        """test fake user"""
-        response = self.client.login(username="fake", password="fake")
-        self.assertFalse(response)
 
 class OsTest(TestCase):
     """Class os"""
