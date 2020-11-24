842c7bf0913b07f2a1f4e72c0c14570104b17345
unknown <jmillot41@gmail.com>
2020-11-24 15:48:46 +0100

    remove print

--- manage_user/views.py
+++ manage_user/views.py
@@ -37,10 +37,8 @@ def register(request):
     """function for create a account """
     if request.method == "GET":
         return render(request, "signup.html")
-    # print(request.POST["password"])
     if request.method == "POST":
         form = RegistrationForm(request.POST)
-        # print(form.is_valid())
         if form.is_valid():
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password"]
@@ -48,15 +46,10 @@ def register(request):
             try:
                 user = form.save(commit=False)
                 user.set_password(request.POST["password"])
-                # user = User.objects.create_user(username,password,email)
                 user.save()
-                print(user.check_password(password))
-                # print(user)
                 login(request, user)  # nous connectons l'utilisateur
-                print("Votre compte a bien été crée")
             except Exception as e:
                 form = RegistrationForm()
-                print(e)
             return render(request, "login.html")
         else:
             return render(request, "login.html")
@@ -64,13 +57,8 @@ def register(request):
 
 def connexion(request):
     """function who called when a user try to be connected"""
-    print(request.user.is_authenticated)
     if request.user.is_authenticated:
-        print("déja connecté")
         return redirect("index.html")
-    # if request.method == "GET":
-    #     print("Voir la page sans se connecter")
-    #     return render(request, 'login.html')
     if request.method == "POST":
         form = ConnexionForm(request.POST)
         username = request.POST["username"]
@@ -79,23 +67,15 @@ def connexion(request):
         print(password)
         user = authenticate(
             username=username, password=password
-        )  # Nous vérifions si les données sont correctes
-        # print(user)
-        # print(user.check_password(password))
-
+        )
         if user is not None:  # Si l'objet renvoyé n'est pas None
             login(request, user)  # nous connectons l'utilisateur
-            # return redirect(user_page)
-            print("Connecté")
             return redirect("index")
         else:  # sinon une erreur sera affichée
-            print("Utilisateur inconnu ou mauvais de mot de passe.")
             return redirect("login")
     else:
-        # form = ConnexionForm()
-        print("Rien ne se passe aucune connexion effectué")
-    context = {"ConnexionForm": ConnexionForm()}
-    return render(request, "login.html", context)
+        context = {"ConnexionForm": ConnexionForm()}
+        return render(request, "login.html", context)
 
 
 def logout_view(request):
