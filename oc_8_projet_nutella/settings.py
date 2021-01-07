189a92411ad32ca16c1bd30e1eb75012904ecdd0
Jilvo <jmillot41@gmail.com>
2021-01-07 03:12:29 +0000

    travis

--- oc_8_projet_nutella/settings.py
+++ oc_8_projet_nutella/settings.py
@@ -83,10 +83,11 @@ WSGI_APPLICATION = "oc_8_projet_nutella.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "oc8_nutella_purbeurre",
-        "USER": "postgres",
-        "PASSWORD": "root",
+        "NAME": "oc10_purbeurre",
+        "USER": "jilvo",
+        "PASSWORD": "dijo2112",
         "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
@@ -95,23 +96,12 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_\
-        validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.\
-        MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.\
-        CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.\
-        NumericPasswordValidator",
-    },
-]
+            {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'}, 
+                {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'}, 
+                    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'}, 
+                        {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+                        ]
+
 # Authentication backends
 AUTHENTICATION_BACKENDS = (
     'manage_user.backends.EmailAuthBackend',)
